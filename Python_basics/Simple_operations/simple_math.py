d8ac4732d112cd2b7cc93ee7ea3a14143fadfede
BunnyeNyash <otachibosibori@gmail.com>
2023-07-02 20:49:41 +0300

    simple math calculations

---
+++ Python_basics/Simple_operations/simple_math.py
@@ -0,0 +1,22 @@
+#calculation
+
+#addition
+print(2 + 2)
+
+#substraction
+print(10 - 8)
+
+#multiplication
+print(2 * 4)
+
+#division
+print(50 / 5)
+
+#remainder
+print(40 % 3)
+
+#mixed calculation
+print(5 + 4 - 3)
+
+#parantheses to indicate the operations we want performed first
+print(2 * (4 + 6))
