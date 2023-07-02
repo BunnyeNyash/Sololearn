bbe898668168a63116263cbb1b72a46ce91c1635
BunnyeNyash <otachibosibori@gmail.com>
2023-07-02 20:58:07 +0300

    Python program to calculate how many second there are in 30 days

---
+++ Python_basics/Simple_operations/seconds_in_days.py
@@ -0,0 +1,11 @@
+#calculation
+seconds_per_minute = 60
+minutes_per_hour = 60
+hours_per_day = 24
+days = 30
+
+seconds = seconds_per_minute * minutes_per_hour * hours_per_day * days
+
+#output
+print("There are", seconds, "seconds in 30 days.")
+
